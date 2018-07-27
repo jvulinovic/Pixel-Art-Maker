5ee7ba1da71ffb15b58b59d4bb4f378911c088a3
Jadran Vulinovic <jvulinovic@gmail.com>
2018-07-27 16:19:16 +1000

    First commit - canvas creator completed

--- designs.js
+++ designs.js
@@ -3,8 +3,37 @@
 
 // When size is submitted by the user, call makeGrid()
 
+
+const submitButton = document.querySelector('.submit');
+const canvas = document.querySelector('table');
+
+
 function makeGrid() {
-
-// Your code goes here!
-
+  submitButton.addEventListener('click', function () {
+  createCanvas();
+});
 }
+
+function reset () {
+  document.querySelector('tbody').remove();
+}
+
+function createCanvas () {
+  reset();
+  const createbody = document.createElement('tbody');
+  canvas.prepend(createbody);
+  const ih = document.querySelector('#inputHeight').value;
+  const iw = document.querySelector('#inputWidth').value;
+  for (a = 1; a<=ih; a++) {
+      const newRow = document.createElement("tr");
+      canvas.firstElementChild.prepend(newRow);
+      for (b = 1; b<=iw; b++) {
+        const newColumn = document.createElement("td");
+        newRow.appendChild(newColumn);
+      };
+    };
+}
+
+
+
+makeGrid();
