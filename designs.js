b6473be2ae700980a87ddb47537f86b1469368e1
Jadran Vulinovic <jvulinovic@gmail.com>
2018-07-29 19:29:13 +1000

    Included color picker. Completed version.

--- designs.js
+++ designs.js
@@ -1,39 +1,52 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
-
-
+// button to record height and width selection
 const submitButton = document.querySelector('.submit');
+
+// variable to hold canvas for pixelart
 const canvas = document.querySelector('table');
 
-
+// function start the pixelart maker
 function makeGrid() {
+  createPens();
   submitButton.addEventListener('click', function () {
   createCanvas();
 });
 }
 
-function reset () {
-  document.querySelector('tbody').remove();
-}
+// creates the canvas / clears canvas if pixelart already exists
 
 function createCanvas () {
-  reset();
+  document.querySelector('tbody').remove();
   const createbody = document.createElement('tbody');
   canvas.prepend(createbody);
   const ih = document.querySelector('#inputHeight').value;
   const iw = document.querySelector('#inputWidth').value;
-  for (a = 1; a<=ih; a++) {
+  // iterate through table to create the requested matrix
+  for (row = 1; row<=ih; row++) {
       const newRow = document.createElement("tr");
       canvas.firstElementChild.prepend(newRow);
-      for (b = 1; b<=iw; b++) {
+      for (col = 1; col<=iw; col++) {
         const newColumn = document.createElement("td");
         newRow.appendChild(newColumn);
       };
     };
 }
 
+// creates the eventlisteners for coloring and color removal (double click reverts square to white)
 
+function createPens () {
+  canvas.addEventListener('click', function (event) {
+      current = event.target;
+      if (event.target.tagName === 'TD') {
+        current.style.backgroundColor = colorPicker.value;
+      }
+    });
+  canvas.addEventListener('dblclick', function (event) {
+      current = event.target;
+      if (event.target.tagName === 'TD') {
+        current.style.backgroundColor = "white";
+      }
+    });
+}
 
 makeGrid();
